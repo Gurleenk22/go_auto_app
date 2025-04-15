4b425894965334be489b97e988217595895df5de
Gurleenk22 <gkaur22@norquest.ca>
2025-04-15 17:53:05 -0600

    Add files via upload

--- 3830app.py
+++ 3830app.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1FhMrVy-O4WYmmJsrimqK9_xvatgKvCtp
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
